a4e7ec0e3f5290756904697dae90c5ca0ecf87d0
mat <mat@mignin.com>
2020-06-29 14:03:26 -0400

    stop upload folders from erasing during export

--- webapp/templates/changename.sh
+++ webapp/templates/changename.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cd $1
+mv ../static/uploads assets/uploads/
 rm -rf ../static/
 mv assets/ ../static/
-mkdir ../static/uploads
-mkdir ../static/uploads/mat
-mkdir ../static/uploads/mike
-mkdir ../static/uploads/tony
-mkdir ../static/uploads/christie
-mkdir ../static/uploads/que 
+#mkdir ../static/uploads
+#mkdir ../static/uploads/mat
+#mkdir ../static/uploads/mike
+#mkdir ../static/uploads/tony
+#mkdir ../static/uploads/christie
+#mkdir ../static/uploads/que 
 sed -i'.bs' -e 's|assets/|static/|g' *.html
 rm -f *.html.bs
