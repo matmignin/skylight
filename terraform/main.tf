f197b12e1e438ca75061fde81cd1a38fdba949cc
mat <mat@mignin.com>
2020-07-06 16:27:31 -0400

    fix branch timeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,28 @@
 provider "aws" {
   profile = "default"
-  region  = var.region
-  #version = "~> 2.0"
+  region  = "us-east-1"
 }
-
 
 resource "aws_instance" "skylight_instance" {
-  key_name               = "skylight"
-  ami                    = "ami-2757f631"
-  instance_type          = var.server_instance_type
-  vpc_security_group_ids = [aws_security_group.skylight_sg.id]
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+  key_name      = "skylight"
 
-  tags = {
-    name = "skylight-tag"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get -y update",
-      "sudo apt-get install -y python",
-    ]
-    connection {
-      type = "ssh"
-      user = "ubuntu"
-      #private_key = file("~/.ssh/skylight.pem")
-      host = self.public_ip
-    }
-  }
+  #provisioner "remote-exec" {
+  #inline = [
+  #"sudo apt-get -y update",
+  #"sudo apt-get install -y python",
+  #]
+  #connection {
+  #type = "ssh"
+  #user = "ubuntu"
+  #}
+  #}
 
 }
 
-resource "aws_security_group" "skylight_sg" {
-  name = "skylight-sg"
-
+resource "aws_security_group" "skylight_elb" {
+  name = "skylight-elb"
 
   egress {
     from_port   = 0
@@ -41,14 +31,14 @@ resource "aws_security_group" "skylight_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress { # http port
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress { # https port
+  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
@@ -56,7 +46,7 @@ resource "aws_security_group" "skylight_sg" {
   }
 
 
-  ingress { #ssh port
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -64,9 +54,32 @@ resource "aws_security_group" "skylight_sg" {
   }
 }
 
+resource "aws_elb" "skylight_lb" {
+  name               = "skylight-lb"
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  instances          = ["${aws_instance.skylight_instance.id}"]
 
-output "instance_ips" {
-  value = [aws_instance.skylight_instance.public_ip]
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:80/"
+  }
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 4
 }
 
-
+output "instance_ips" {
+  value = ["${aws_instance.skylight_instance.*.public_ip}"]
+}
